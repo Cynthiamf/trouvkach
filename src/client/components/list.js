5c37c7e1edc168ec415140f3b3b9c2fe33c060fb
Lionel <lionel.franco2310@gmail.com>
2019-09-17 14:21:53 +0200

    Recup des données et affichage

--- src/client/components/list.js
+++ src/client/components/list.js
@@ -8,14 +8,49 @@
 
 import * as React from "react";
 
-const List = () => (
-    <div>
-        <ul>
-            <li>{"Bank's name"}</li>
-            <li>{"Terminal's address"}</li>
-            <li>{"Bank's url"}</li>
-        </ul>
-    </div>
-);
+class List extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            terminals: [],
+            banks: [],
+        };
+    }
+    componentDidMount() {
+        fetch("/api/terminals")
+            .then(res => res.json())
+            .then(terminals => {
+                this.setState({terminals});
+            });
+        fetch("/api/banks")
+            .then(res => res.json())
+            .then(banks => {
+                this.setState({banks});
+            });
+    }
+    render() {
+        return (
+            <div>
+                <ul>
+                    {this.state.banks.map(banks => (
+                        <li key={banks.name}>
+                            {"Name:"} {banks.name}
+                        </li>
+                    ))}
+                    {this.state.terminals.map(terminals => (
+                        <li key={terminals.address}>
+                            {"Address:"} {terminals.address}
+                        </li>
+                    ))}
+                    {this.state.banks.map(banks => (
+                        <li key={banks.url}>
+                            {"Website:"} {banks.url}
+                        </li>
+                    ))}
+                </ul>
+            </div>
+        );
+    }
+}
 
 export default List;
