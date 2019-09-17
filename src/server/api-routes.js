9fccd8d7d1d0ead10673bfc7ee5c68c3a5672947
Lionel <lionel.franco2310@gmail.com>
2019-09-17 11:51:18 +0200

    Maj routes

--- src/server/api-routes.js
+++ src/server/api-routes.js
@@ -1,6 +1,21 @@
 const mongo = require("mongodb").MongoClient;
+//const ObjectID = require("mongodb").ObjectID;
 const router = require("express").Router();
 const url = "mongodb://dev:dev@mongo:27017/admin";
+
+async function installationMongo(term) {
+    const items = await term.find({}).toArray();
+    for (const [i, element] of items.entries()) {
+        element.location = {
+            type: "Point",
+            coordinates: [element.longitude, element.latitude],
+        };
+        console.log(element);
+        console.log(i);
+        term.updateOne({_id: element._id}, {$set: element});
+    }
+    await term.createIndex({location: "2dsphere"});
+}
 
 mongo.connect(
     url,
@@ -27,10 +42,40 @@ mongo.connect(
         });
 
         router.get("/terminals", (req, res) => {
-            terminals.find().toArray((_err2, items) => {
+            terminals.find().toArray((_err, items) => {
                 res.json(items);
+            });
+        });
+
+        router.get("/terminals/:long/:lat", (req, res) => {
+            console.log(req.params.long, req.params.lat);
+            terminals
+                .aggregate([
+                    {
+                        $geoNear: {
+                            near: {
+                                type: "Point",
+                                coordinates: [
+                                    parseFloat(req.params.long),
+                                    parseFloat(req.params.lat),
+                                ],
+                            },
+                            distanceField: "dist.calculated",
+                            maxDistance: 2000,
+                        },
+                    },
+                ])
+                .toArray((_err, items) => {
+                    res.json(items);
+                });
+        });
+
+        router.get("/test/", (req, res) => {
+            installationMongo(terminals).then(() => {
+                res.json("ok");
             });
         });
     },
 );
+
 module.exports = router;
