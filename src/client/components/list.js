99bf4a5e185982114baf0d8d449709a05bcd01f5
Cynthiamf <c_martiny@outlook.com>
2019-09-18 14:45:35 +0200

    récup des coordonnées

--- src/client/components/list.js
+++ src/client/components/list.js
@@ -7,6 +7,7 @@
  */
 
 import * as React from "react";
+
 class List extends React.Component {
     constructor() {
         super();
@@ -20,12 +21,18 @@ class List extends React.Component {
             .then(res => res.json())
             .then(terminals => {
                 this.setState({terminals});
+                console.log(terminals[0].location.coordinates);
             });
         fetch("/api/banks")
             .then(res => res.json())
             .then(banks => {
                 this.setState({banks});
             });
+        // fetch("/api/terminals/:long/:lat")
+        //     .then(res => res.json())
+        //     .then(terminals.location.coordinates => {
+        //         this.setState({coordinates});
+        //     });
     }
     render() {
         return (
@@ -40,6 +47,9 @@ class List extends React.Component {
                         <li>
                             {terminal.bankDetails[0] &&
                                 terminal.bankDetails[0].url}
+                        </li>
+                        <li>
+                            {terminal.location && terminal.location.coordinates}
                         </li>
                     </ul>
                 ))}
